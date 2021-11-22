142aa8b50750952d42f245283dba113746bebb35
ferdouszislam <ferdouszislam@gmail.com>
2021-11-22 19:28:12 +0600

    update applyML util with showEvaluationGraph_regression() #9
    graph for showing r2 scores on the train set for regression added.

--- Notebooks/utils/applyML_util.py
+++ Notebooks/utils/applyML_util.py
@@ -17,8 +17,7 @@ def train_regression(model, param_grid, df, cls):
   parameters- 
   @model: object of model class, 
   @param_grid: dictonary of hyper-parameters to be checked
-  @X_train: features int the train dataset
-  @y_train: output class in the train dataset
+  @df: train dataset
 
   returns- 
   @model: the best model found by GridSearchCV and 10-fold cross validation
@@ -52,8 +51,7 @@ def eval_regression(model, df, cls):
 
   parameters-
   @model: selected object of the Model class
-  @X_test: features in the test dataset
-  @y_test: output class in the test dataset
+  @df: test dataset
 
   returns-
   @test_r2: r2 score in the test set
@@ -70,6 +68,53 @@ def eval_regression(model, df, cls):
   test_rmse = round(sqrt(mean_squared_error(y_test, y_test_pred)), 4)
 
   return test_r2, test_mae, test_rmse
+
+
+def showEvalutationGraph_regression(ModelClass, df, cls, 
+                                    x_axis_param_name, x_axis_param_vals, selected_model_params):
+  """
+  generate graph with r2 scores of the model on the train set
+  for different values of a hyper-parameter
+
+  parameters-
+  @model: the Model class itself (not an object)
+  @df: features in the train dataset
+  @cls: output class in the train dataset
+  @x_axis_param_name: the hyper-parameter name to be shown on the x_axis
+  @x_axis_param_vals: list of hyper-parameter values
+  @selected_model_params: dictonary of parameters for the model selected by GridSearchCV
+                          also pass in parameters like 'random_state' and 'n_jobs' if required 
+  """
+
+  X = df.drop(columns=cls)
+  y = df[cls]
+  cv = KFold(n_splits=10, random_state=RAND_SEED, shuffle= True)
+
+  r2s = []
+
+  selected_x_axis_param_val = selected_model_params[x_axis_param_name]
+
+  # sort the x axis parameters so the graph looks appropriate
+  x_axis_param_vals.sort()
+
+  for x_axis_param_val in x_axis_param_vals:
+    model_params = selected_model_params
+    model_params[x_axis_param_name] = x_axis_param_val
+
+    model = ModelClass(**model_params)
+
+    r2_segments = cross_val_score(model, X, y, scoring='r2',cv=cv, n_jobs=1)
+    r2s.append(np.mean(r2_segments))
+
+  plt.figure(figsize =(15,9))
+  plt.plot(x_axis_param_vals, r2s, 'ro-')
+  plt.axvline(x=selected_x_axis_param_val, color='k', linestyle='--')
+  plt.legend(['R2-score', f'selected value ({x_axis_param_name}={selected_x_axis_param_val})'], fontsize=16)
+  plt.xlabel(x_axis_param_name, fontsize=18)
+  plt.ylabel('R2-score', fontsize=18)
+  plt.xticks(fontsize=18)
+  plt.yticks(fontsize=18)
+  plt.show()
 
 
 def foo():
